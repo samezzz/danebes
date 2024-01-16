d3564c4930ea64b7eceff7b412fad7be65bc833d
Samuel Essilfie <PsalmuelSelfie@gmail.com>
2024-01-16 01:10:16 +0000

    style: changed font and removed button

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -5,30 +5,28 @@ import { services } from "@/constants";
 import ServiceCard from "./ServiceCard";
 
 const Business = () => {
-  return (
-    <section id="services" className={`${layout.section}`}>
-      <div className={`${layout.sectionInfo}`}>
-        <h2 className={`${styles.heading2}`}>
-          Transforming Healthcare Solutions with Precision and Quality
-          <br className="sm:block hidden" />
-        </h2>
-        <p className={`${styles.paragraph}text-neutral-600 dark:text-neutral-300 max-w-[470px] mt-5`}>
-          We deliver comprehensive Biomedical Engineering solutions, including
-          precision equipment, tailored services, and innovative healthcare
-          advancements.
-        </p>
+	return (
+		<section id="services" className={`${layout.section}`}>
+			<div className={`${layout.sectionInfo}`}>
+				<h2 className={`${styles.heading2}`}>
+					Transforming Healthcare Solutions with Precision and Quality
+					<br className="sm:block hidden" />
+				</h2>
+				<p
+					className={`${styles.paragraph}text-neutral-600 dark:text-neutral-300 max-w-[470px] mt-5`}
+				>
+					We deliver comprehensive Biomedical Engineering solutions, including precision
+					equipment, tailored services, and innovative healthcare advancements.
+				</p>
+			</div>
 
-        <Button styles={`mt-10`} />
-      </div>
-
-      <div className={`${layout.sectionImg} flex-col`}>
-        {services.map((service, index) => {
-          return (
-          <ServiceCard key={index} items={service}/>
-        )})}
-      </div>
-    </section>
-  );
+			<div className={`${layout.sectionImg} flex-col`}>
+				{services.map((service, index) => {
+					return <ServiceCard key={index} items={service} />;
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Business;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,101 +1,97 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class"],
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-    },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        // primary: "#00040f",
-        // secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          // @ts-expect-error
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          // @ts-expect-error
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "ping-once": "ping 5s cubic-bezier(0, 0, 0.2, 1)",
-        "rotating": "rotating 30s linear infinite",
-        "spin-1.5": "spin 1.5s linear infinite",
-        "spin-2": "spin 2s linear infinite",
-        "spin-3": "spin 3s linear infinite",
-      },
-    },
-    screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
-    },
-  },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+		},
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			colors: {
+				// primary: "#00040f",
+				// secondary: "#00f6ff",
+				dimWhite: "rgba(255, 255, 255, 0.7)",
+				dimBlue: "rgba(9, 151, 124, 0.1)",
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			keyframes: {
+				"accordion-down": {
+					// @ts-expect-error
+					from: { height: 0 },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					// @ts-expect-error
+					to: { height: 0 },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+				"ping-once": "ping 5s cubic-bezier(0, 0, 0.2, 1)",
+				rotating: "rotating 30s linear infinite",
+				"spin-1.5": "spin 1.5s linear infinite",
+				"spin-2": "spin 2s linear infinite",
+				"spin-3": "spin 3s linear infinite",
+			},
+		},
+		screens: {
+			xs: "480px",
+			ss: "620px",
+			sm: "768px",
+			md: "1060px",
+			lg: "1200px",
+			xl: "1700px",
+		},
+	},
+	plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 export default config;
