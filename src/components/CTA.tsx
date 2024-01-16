3ba562024db93e5965fe0f69d352662ae22a6a46
Samuel Essilfie <PsalmuelSelfie@gmail.com>
2024-01-16 01:43:48 +0000

    style: added margin

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -11,7 +11,7 @@ const CTA = () => {
 			<div className="flex-1 flex flex-col">
 				<h2 className={styles.heading2}>Try our service now!</h2>
 				<p
-					className={`${styles.paragraph} text-neutral-600 dark:text-neutral-300 max-w-[470px] mt-5`}
+					className={`${styles.paragraph} text-neutral-600 dark:text-neutral-300 max-w-[470px] mt-5 mb-4`}
 				>
 					Let us take care of your Biomedical Engineering needs while you focus on what matters.
 				</p>
